c15f49cf4bad1c61c84cf8cd9a5182f43343c9c4
Jeremy Marchand <kodlian@users.noreply.github.com>
2016-08-17 21:36:45 +0200

    Disable AlamofireTest

--- AlamofireXMLRPCTests/AlamofireXMLRPCTests.swift
+++ AlamofireXMLRPCTests/AlamofireXMLRPCTests.swift
@@ -23,19 +23,19 @@ class AlamofireXMLRPCTests: XCTestCase {
         super.tearDown()
     }
 
-    func testRPCXMLCall() {
-        let expt = self.expectationWithDescription("XMLRPCCall")
-
-        let data = "thiswillbencoded".dataUsingEncoding(NSUTF8StringEncoding)!
-
-        let params: [Any] = [42,"string",3.44, NSDate(), data,[1,2,3,5.0,"substring", XMLRPCArray([1])] as XMLRPCArray,["key":"value","keyint":2] as XMLRPCStructure]
-
-        request("http://localhost:8888/xmlrpc", methodName: "testMethod", parameters: params).responseXMLRPC { (response: Response<XMLRPCNode, XMLRPCError>) -> Void in
-            XCTAssert(response.result.value?.count > 0, "")
-
-            expt.fulfill()
-        }
-
-        self.waitForExpectationsWithTimeout(4, handler: nil)
-    }
+//    func testRPCXMLCall() {
+//        let expt = self.expectationWithDescription("XMLRPCCall")
+//
+//        let data = "thiswillbencoded".dataUsingEncoding(NSUTF8StringEncoding)!
+//
+//        let params: [Any] = [42,"string",3.44, NSDate(), data,[1,2,3,5.0,"substring", XMLRPCArray([1])] as XMLRPCArray,["key":"value","keyint":2] as XMLRPCStructure]
+//
+//        request("http://localhost:8888/xmlrpc", methodName: "testMethod", parameters: params).responseXMLRPC { (response: Response<XMLRPCNode, XMLRPCError>) -> Void in
+//            XCTAssert(response.result.value?.count > 0, "")
+//
+//            expt.fulfill()
+//        }
+//
+//        self.waitForExpectationsWithTimeout(4, handler: nil)
+//    }
 }
