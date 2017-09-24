fb5d1c7cfba659ed3678b9ac656cb9352ffb9d48
Jérémy Marchand <contact@kodlian.com>
2017-09-24 17:22:34 +0200

    Package version 3 for compatibility

--- Package.swift
+++ Package.swift
@@ -1,29 +1,13 @@
-// swift-tools-version:4.0
+// swift-tools-version:3.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "AlamofireXMLRPC",
-    products: [
-        .library(
-            name: "AlamofireXMLRPC",
-            targets: ["AlamofireXMLRPC"]),
-    ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire", from: "4.0.0"),
-        .package(url: "https://github.com/tadija/AEXML.git", from: "4.0.0")
-    ],
-    targets: [
-        .target(
-            name: "AlamofireXMLRPC",
-            dependencies: [],
-            path:"Sources"
-        ),
-        .testTarget(
-            name: "AlamofireXMLRPCTests",
-            dependencies: ["AlamofireXMLRPC"],
-            path:"Tests"
-        ),
+        .Package(url: "https://github.com/Alamofire/Alamofire", majorVersion: 4),
+        .Package(url: "https://github.com/tadija/AEXML.git", majorVersion: 4)
     ]
 )
+
