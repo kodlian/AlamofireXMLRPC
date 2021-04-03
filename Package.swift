e16a0dfd226ba7d9cc09c7c6f59ca3f46ec6382c
Jonathan Foster <jonathan@jonathanfoster.io>
2021-04-03 19:38:38 -0400

    Update to AEXML 4.6, Alamofire 5.4.2, and Swift 5

--- Package.swift
+++ Package.swift
@@ -1,12 +1,10 @@
-// swift-tools-version:3.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
     name: "AlamofireXMLRPC",
     dependencies: [
-        .Package(url: "https://github.com/Alamofire/Alamofire", majorVersion: 4),
+        .Package(url: "https://github.com/Alamofire/Alamofire", majorVersion: 5),
         .Package(url: "https://github.com/tadija/AEXML.git", majorVersion: 4)
     ]
 )
